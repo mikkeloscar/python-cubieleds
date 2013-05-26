8ee531590c250accc7db8c4381a9b12c7087f577
Mikkel Oscar Lyderik <mikkeloscar@gmail.com>
2013-05-27 01:21:29 +0200

    Added classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 from setuptools import setup, Extension, find_packages
 
+classifiers = ['Development Status :: 4 - Beta',
+               'Operating System :: POSIX :: Linux',
+               'License :: OSI Approved :: GNU General Public License (GPL)',
+               'Intended Audience :: Developers',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3',
+               'Topic :: Software Development',
+               'Topic :: System :: Hardware']
+
+
 setup(name          = 'Cubie.leds',
       version       = '0.1',
       author        = 'Mikkel Oscar Lyderik',
@@ -7,6 +17,8 @@ setup(name          = 'Cubie.leds',
       description   = 'A module to control Cubieboard LEDs',
       license       = 'GPL',
       keywords      = 'Cubie Board leds',
+      url           = 'https://github.com/mikkeloscar/python-cubieleds',
+      classifiers   = classifiers,
       packages      = find_packages(),
       ext_modules   = [Extension('Cubie.leds', ['src/py_leds.c',
                                                 'src/leds.c'])]
