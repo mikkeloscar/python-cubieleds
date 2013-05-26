49cc377513e15b7691cb8e82b2b044758e5c44c2
Mikkel Oscar Lyderik <mikkeloscar@gmail.com>
2013-05-26 15:00:20 +0200

    Added proper GPL license

--- src/led.c
+++ src/led.c
@@ -1,18 +1,18 @@
 /**
- * This program is free software; you can redistribute it and/or modify
+ * Copyright (C) 2013  Mikkel Oscar Lyderik
+ * 
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright (C) Mikkel Oscar Lyderik, 2013
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #define _GNU_SOURCE
@@ -23,29 +23,31 @@
 
 int main()
 {
-    struct leds_t blue, green;
+    struct leds_t leds;
 
-    if (leds_init(&blue, "blue") < 0)
+    if (leds_init(&leds) < 0)
         return 1;
-    if (leds_init(&green, "green") < 0)
-        return 1;
-
     
     for (int i=0; i < 20; i++) {
         if (i % 2 == 0) {
-            leds_on(&blue);
-            leds_off(&green);
+            leds_on(&leds, "blue");
+            leds_off(&leds, "green");
         } else {
-            leds_off(&blue);
-            leds_on(&green);
+            leds_off(&leds, "blue");
+            leds_on(&leds, "green");
         }
         usleep(1000*100);
     }
 
-    leds_off(&blue);
-    leds_off(&green);
+    leds_off(&leds, "blue");
+    leds_off(&leds, "green");
 
-    leds_trigger(&blue, "usb-online");
+    leds_trigger(&leds, "blue", "usb-online");
+
+    char trigger[1024];
+    leds_trigger_status(&leds, "blue", trigger);
+
+    printf("trigger: %s\n", trigger);
 
     return 0;
 }
